e145f772ba55a701053f7c8c7e78995c3a43d7ff
PesAdam <pesadam15@gmail.com>
2021-04-01 20:44:26 +0200

    logout

---
+++ db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `users` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `username` varchar(50) NOT NULL,
+ `email` varchar(50) NOT NULL,
+ `password` varchar(50) NOT NULL,
+ `create_datetime` datetime NOT NULL,
+ PRIMARY KEY (`id`)
+);
