d7fa9cf60679bb04e51a4ed7f425218a4d15d7b6
belo <68499722+PesAdam@users.noreply.github.com>
2022-06-18 23:13:29 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
    
-    $con = mysqli_connect("46.229.230.164","hz024700","dhynydor","hz024703db");
+    $con = mysqli_connect("","0","","");
     
     if (mysqli_connect_errno()){
         echo "Nepodarilo sa mi pripojit do db " . mysqli_connect_error();
